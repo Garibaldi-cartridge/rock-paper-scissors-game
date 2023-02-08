b03bce395dbb2b2ac0d49034d8d98adb01db34e2
Garibaldi-cartridge <jonathan.r.fear@gmail.com>
2023-02-08 07:34:32 +0000

    try again

--- script.js
+++ script.js
@@ -1,71 +1,73 @@
 
-//Declare gameChoices as an array.
-const gameChoices = ['Rock', 'Paper', 'Scissors']
 
-//declare get getComputerChoice as a function
-function getComputerChoice() {
-// math.random chooses a random number between 0 and 1 (not including 1)
-//gameChoices.length multiplies this by the length of the array.
-//math.floor returns whole numbers
-//this is applied to the gameChoices array.
-    return gameChoices[Math.floor(Math.random() * gameChoices.length)];
-}
+const gameChoices = ['Rock', 'Paper', 'Scissors']
+let roundCounter = 0;
 let roundNumber = 1;
 let playerWins = 0;
 let computerWins = 0;
 
-function playRound(playerSelection, computerSelection) {
+function getComputerChoice() {
 
-    
+    return gameChoices[Math.floor(Math.random() * gameChoices.length)];
+}
+
+function playRound(playerSelection, computerSelection) {
 
     if (playerSelection === computerSelection) {
         alert('You Draw!');
-    }
-    else if (playerSelection==='Rock'&&computerSelection==='Paper') {
+    }     else if (playerSelection==='Rock'&&computerSelection==='Paper') {
         alert ('You Lose! Paper beats Rock!');
         computerWins ++;
-    }
-    else if (playerSelection==='Rock'&&computerSelection==='Scissors') {
+    }     else if (playerSelection==='Rock'&&computerSelection==='Scissors') {
         alert ('You Win! Rock beats Scissors!')
         playerWins ++;
-    }
-    else if (playerSelection==='Paper'&&computerSelection==='Scissors') {
+    }     else if (playerSelection==='Paper'&&computerSelection==='Scissors') {
         alert ('You Lose! Scissors beats paper!');
         computerWins ++;
-    }
-    else if (playerSelection==='Paper'&&computerSelection==='Rock') {
+    }    else if (playerSelection==='Paper'&&computerSelection==='Rock') {
         alert ('You Win! Paper beats Rock!');
         playerWins ++;
-    }
-    else if (playerSelection==='Scissors'&&computerSelection==='Rock') {
+    }    else if (playerSelection==='Scissors'&&computerSelection==='Rock') {
         alert ('You Lose! Rock beats Scissors!');
         computerWins ++;
-    }
-    else if (playerSelection==='Scissors'&&computerSelection==='Paper') {
+    }    else if (playerSelection==='Scissors'&&computerSelection==='Paper') {
         alert ('You Win! Scissors beats Paper!');
         playerWins ++;
     }
-    roundNumber ++ ; 
-    (`Current score is Player: ${playerWins} Computer: ${computerWins}`);
+    roundCounter ++ ; 
 }
-const roundCounter = 0;
 
-function game() {
-    for (let i=0; i<5; i++) {
-    let playerSelection = prompt('Rock Paper or Scissors?');
+function game(example) {
+    
+    let playerSelection = example;
     playRound(playerSelection, getComputerChoice());
+    resultsTracker.textContent = `Round ${roundCounter}. Current score is Player: ${playerWins} Computer: ${computerWins} \n`;
+    
+    if (roundCounter===5) {
+        if (playerWins>computerWins) {
+            alert (`You Win! Player: ${playerWins} Computer: ${computerWins}`);
+            }
+        else if (computerWins>playerWins) {
+                alert (`You Lose! Player: ${playerWins} Computer: ${computerWins}`);
+            }
+        else if (computerWins===playerWins) {
+                alert (`It's a Draw! Player: ${playerWins} Computer: ${computerWins}`);
+            }
+        }
     }
-    if (playerWins>computerWins) {
-    alert (`You Win! Player: ${playerWins} Computer: ${computerWins}`);
-    }
-    else if (computerWins>playerWins) {
-        alert (`You Lose! Player: ${playerWins} Computer: ${computerWins}`);
-    }
-    else if (computerWins===playerWins) {
-        alert (`It's a Draw! Player: ${playerWins} Computer: ${computerWins}`);
-    }
-    }
+const rockBtn = document.querySelector('#rock');
+rockBtn.addEventListener('click', () => {
+    game('Rock');
+});
+const paperBtn = document.querySelector('#paper');
+paperBtn.addEventListener('click', () => {
+    game('Paper');
+});
+const scisBtn = document.querySelector('#scissors');
+scisBtn.addEventListener('click', () => {
+    game('Scissors');
+});
 
+const resultsTracker = document.querySelector('#resultsTracker');
 const computerSelection = getComputerChoice();
-console.log(game());
 
